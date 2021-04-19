8eeecf04dae3ce4feefe2006169ba6669ae9ce49
anirudh <64965976+anirudh8303@users.noreply.github.com>
2021-04-19 20:02:08 +0530

    Stock app done

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StockScreen from "./screens/StockScreen"
+import { SafeAreaView } from 'react-native'
+import { ScrollView } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style="dark" hidden={true}/>
+    <SafeAreaView style={{flex:1, backgroundColor: "white"}}>
+    <ScrollView style={styles.container}>
+      <StockScreen />
+    </ScrollView>
+    </SafeAreaView>
+    </>
   );
 }
 
@@ -15,7 +22,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
